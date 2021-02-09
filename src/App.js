04e1203532b55e1b5fb263b91c93054df09d9164
Rémi Michel <remi.michel38@gmail.com>
2021-02-09 13:20:59 +0100

    feat(): Session du 09/02

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import {useState} from 'react';
+
+function LocationCard(props) {
+    return <div>
+        <h1>{props.location.title}</h1>
+        <img src={props.location.pictures[0]}/>
     </div>
-  );
+}
+
+
+function App() {
+
+    const [locations, setLocations] = useState([]);
+
+    fetch('/locations.json')
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            setLocations(data)
+        })
+
+
+    return (
+        <div className="App">
+            {locations.map((location) => <LocationCard key={location.id} location={location}/>)}
+            
+        </div>
+    );
 }
 
 export default App;
